42f82faa76dc99c047073f6df5e6b0e3f2f9ac71
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-07-22 11:55:12 +0300

    first commit

---
+++ circle_area.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import math as m
+
+def circleArea():
+    radius = float(input("Radius: "))
+    radiusSqr = m.pow(radius, 2)
+    area = m.pi * radiusSqr
+    return area
+
+area = circleArea()
+print(area)
